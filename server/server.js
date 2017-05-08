3bf0f748911cb2823b7bcc451ca0a7c135dd5b81
MBD_Ass1 <chairus06@gmail.com>
2017-05-08 17:15:53 +0930

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var routes = require('../routes/index');
+
+const path = require('path');
+
+var app = express();
+var port = 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(routes);
+
+app.listen(port, () => {
+    console.log(`Server has started. Listening on PORT ${port}`);
+});
