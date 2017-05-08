7539e87216431c89eca2412e6a52f5325ee7e774
MBD_Ass1 <chairus06@gmail.com>
2017-05-08 21:09:39 +0930

    Add greeting to new user, and alert others

--- server/server.js
+++ server/server.js
@@ -17,17 +17,38 @@ app.use(routes);
 io.on('connection', (socket) => { // In this case we are listening for events that wants to connect to the server
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createAt: new Date().toDateString()
+    })
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user has joined',
+        createAt: new Date().toDateString()
+    })
+
     socket.on('createMessage', (newMessage) => {
         console.log('New message received', newMessage);
         /*
             NOTE: socket.emit() only emit/send messages to only one specific connection, whereas
-                  io.emit() send messages to all users that are currently connected to the server.
+                  io.emit() send messages to all clients that are currently connected to the server,
+                  including the client that emitted the event. However, socket.broadcast.emit() works
+                  similarly to io.emit() but it doesn't send the emitted message to the sender. In other
+                  words the server only emits the event to all connected clients except the client that
+                  emitted the event.
          */
         io.emit('newMessage', {
             from: newMessage.from,
             text: newMessage.text,
             createdAt: new Date().toDateString()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: newMessage.from,
+        //     text: newMessage.text,
+        //     createdAt: new Date().toDateString()
+        // });
     });
 
     socket.on('disconnect', () => {
